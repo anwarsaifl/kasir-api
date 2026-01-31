559738153fa429a83ad67a51eec6b8f6ff1f236b
Anwar S <anwarsaifullah+code@yandex.com>
2026-02-01 05:16:57 +0700

    add env reader

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	viper "github.com/spf13/viper"
+)
+
 type Config struct {
 	Port string `mapstructure:"PORT"`
+}
+
+func main() {
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
+
+	config := Config{
+		Port: viper.GetString("PORT"),
+	}
+
+	addr := "0.0.0.0:" + config.Port
+	fmt.Println("Server running on " + addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Println("Server failed to run", err)
+	}
 }
